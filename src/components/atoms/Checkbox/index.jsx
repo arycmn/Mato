f160cb42c141df0d4425de6eed54398bd7bd1f4a
ingridbgr <ingridbgr@outlook.com>
2021-01-12 10:39:46 -0300

    feat: fix icons names and added style

--- src/components/atoms/Checkbox/index.jsx
+++ src/components/atoms/Checkbox/index.jsx
@@ -1,9 +1,14 @@
 import { Container } from "./style";
+import { checkedIcon, uncheckedIcon } from "../../../icons";
 
-const Checkbox = ({ children, onClick, icon, type }) => {
+const Checkbox = ({ onClick, status }) => {
   return (
-    <Container onClick={onClick} type={type}>
-      {type === "icon" ? <img src={icon} alt="Button" /> : children}
+    <Container onClick={onClick} status={status}>
+      {status === "checked" ? (
+        <img src={checkedIcon} alt="Checked" />
+      ) : (
+        <img src={uncheckedIcon} alt="unchecked" />
+      )}
     </Container>
   );
 };
