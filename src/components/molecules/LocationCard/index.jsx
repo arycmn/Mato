f0b21bed4a1032fe5b4b9b01ca27349b8b840ae7
Bruno Gehlen <tousepc@gmail.com>
2021-01-13 08:06:49 -0300

    Feat:Add Location card

--- src/components/molecules/LocationCard/index.jsx
+++ src/components/molecules/LocationCard/index.jsx
@@ -1,14 +1,33 @@
-import { Container } from "./style";
+import { Container,LocationName,LocationDescription } from "./style";
 import Picture from '../../atoms/Picture'
+import Button from '../../atoms/Button'
 
-const LocationCard = ({ props }) => {
+const LocationCard = ({ src,locationName, locationDescription }) => {
   return (<Container>
       <Picture
-        src={'https://media-cdn.tripadvisor.com/media/photo-s/17/a1/89/53/doward-park-campsite.jpg'}
+        src={src}
         pictureType={'location'}
         height={'100px'}
         width={'100px'}
       />
+      <LocationName>{locationName.length > 13 ? 
+        `${locationName.trim()
+          .split('')
+          .splice(0,13)
+          .join("")
+          .trim()}...` : 
+          locationName
+        }
+      </LocationName>
+      <LocationDescription>{locationDescription.length > 25 ? 
+              `${locationDescription.trim()
+                .split('')
+                .splice(0,30)
+                .join("")
+                .trim()}...` : 
+                locationDescription
+              }</LocationDescription>
+      <Button>Learn More</Button>
   </Container>);
 };
 
--- src/components/molecules/LocationCard/style.js
+++ src/components/molecules/LocationCard/style.js
@@ -1,5 +1,25 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    width:348px;
+    height:110px;
+    button{
+        margin-left:120px;
+        margin-top:8px;
+    }
+`;
+export const LocationName = styled.div`
+    font-size:18px;
+    padding:0 6px;
+    color: #252C1F;
+    margin-top:-100px;
+    margin-left:100px;
+`
 
+export const LocationDescription = styled.div`
+    color: #252C1F;
+    opacity:0.8;
+    margin-left:110px;
+    width:180px;
+    overflow-wrap: break-word;
 `;
