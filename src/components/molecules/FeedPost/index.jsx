7babf860c97dd0684dae4a7ea860cd8f5e7e7bdc
Bruno Gehlen <tousepc@gmail.com>
2021-01-13 11:04:43 -0300

    Feat: Add src image props

--- src/components/molecules/FeedPost/index.jsx
+++ src/components/molecules/FeedPost/index.jsx
@@ -2,17 +2,17 @@ import {Container,PictureContainer,FavIconContainer} from './style'
 import Picture from '../../atoms/Picture'
 import FavoriteIcon from '../../atoms/FavoriteIcon'
 
-const FeedPost = () => {
+const FeedPost = ({postSrc,profileSrc}) => {
     return(
         <Container>
             <Picture
-                src={'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png'}
+                src={postSrc}
                 width={'340px'}
                 height={'340px'}
                 ></Picture>
             <PictureContainer>
                 <Picture
-                    src={'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png'}
+                    src={profileSrc}
                     width={'72px'}
                     height={'72px'}
                     ></Picture>
