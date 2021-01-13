f4cb915ee24b72257be2458c2c400f97dfc9dcaf
Bruno Gehlen <tousepc@gmail.com>
2021-01-13 14:09:58 -0300

    Feat: Add feed

--- src/components/organisms/Feed/index.jsx
+++ src/components/organisms/Feed/index.jsx
@@ -1,10 +1,17 @@
 import {Container} from './style'
-import
+import FeedPost from '../../molecules/FeedPost'
 
 const Feed = () =>{
     return (
         <Container>
-
+            <FeedPost postSrc={'https://picsum.photos/200'} profileSrc={'https://picsum.photos/200'}/>
+            <FeedPost postSrc={'https://picsum.photos/200'} profileSrc={'https://picsum.photos/200'}/>
+            <FeedPost postSrc={'https://picsum.photos/200'} profileSrc={'https://picsum.photos/200'}/>
+            <FeedPost postSrc={'https://picsum.photos/200'} profileSrc={'https://picsum.photos/200'}/>
+            <FeedPost postSrc={'https://picsum.photos/200'} profileSrc={'https://picsum.photos/200'}/>
+            <FeedPost postSrc={'https://picsum.photos/200'} profileSrc={'https://picsum.photos/200'}/>
+            <FeedPost postSrc={'https://picsum.photos/200'} profileSrc={'https://picsum.photos/200'}/>
+            <FeedPost postSrc={'https://picsum.photos/200'} profileSrc={'https://picsum.photos/200'}/>
         </Container>
     )
 }
