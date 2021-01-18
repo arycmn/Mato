0f767164e5794ef5af59fbfd327eb39dfda320d8
LeandroLino <leao.lino7@gmail.com>
2021-01-18 12:59:12 -0300

    i added img logo

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,12 +1,14 @@
-import Login from "../Login";
+import Login from "../../components/molecules/Login";
 import Title from "../../components/atoms/Title";
 import { Header } from "./style";
+import logo from "../../images/logo Mato.png";
 
 const LoginPage = () => {
   return (
     <Header>
+      <img src={logo}></img>
       <Title text={"Login"} />
-      <Login />;
+      <Login />
     </Header>
   );
 };
