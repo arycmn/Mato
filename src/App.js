65e8acf06420246d280542ac00bd794129cf2932
ingridbgr <ingridbgr@outlook.com>
2021-01-18 10:04:47 -0300

    fix: commit changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import GlobalStyle from "./styles/global";
 import Routes from "./routes";
-import RegisterPage from "./pages/Register";
 
 function App() {
   return (
     <div id="bg-filter">
       <GlobalStyle />
       <Routes />
-      <RegisterPage />
     </div>
   );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,6 @@ import LoginPage from "../pages/Login";
 import RegisterPage from "../pages/Register";
 import UserInterests from "../pages/UserInterests";
 import Location from "../pages/Location";
-<<<<<<< HEAD
-=======
-import Reservation from "../pages/Reservation";
->>>>>>> 4465be916cdd7466313f6d3276732eeef8001ff8
 
 const Routes = () => {
   return (
@@ -25,7 +21,7 @@ const Routes = () => {
       <Route exact path="/search" component={Search} />
       <Route exact path="/news" />
       <Route exact path="/location/:id" component={Location} />
-      <Route exact path="/reservation/:id" component={Reservation} />
+      <Route exact path="/reservation/:id" />
       <Route exact path="/checklist/:id" />
     </Switch>
   );
