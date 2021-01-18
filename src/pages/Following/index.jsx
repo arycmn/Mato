8e5696740e96fc849e3562a4fcdc77fd8d32b211
LeandroLino <leao.lino7@gmail.com>
2021-01-18 14:54:23 -0300

    i fixed the Title, used import title

--- src/pages/Following/index.jsx
+++ src/pages/Following/index.jsx
@@ -2,6 +2,7 @@ import { Container } from "./style.js";
 import FollowingList from "../../components/organisms/FollowingList";
 import MainContent from "../../components/templates/MainContent";
 import FooterMenu from "../../components/molecules/FooterMenu";
+import Title from "../../components/atoms/Title";
 
 const Following = () => {
   const data = [
@@ -25,13 +26,73 @@ const Following = () => {
       name: "Silvio",
       image_url: "https://picsum.photos/200",
     },
+    {
+      name: "Jorge",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Marcos",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Silvia",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Mario",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Silvio",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Jorge",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Marcos",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Silvia",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Mario",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Silvio",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Jorge",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Marcos",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Silvia",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "Mario",
+      image_url: "https://picsum.photos/200",
+    },
+    {
+      name: "AAAA",
+      image_url: "https://picsum.photos/200",
+    },
   ];
-
+  const name = "Lino";
   return (
     <Container>
-      <h1>Welcome $name</h1>
+      <Title text={`Welcome ${name}`}></Title>
       <MainContent>
-        <h2>Following</h2>
+        <Title text={"Following"}></Title>
         <FollowingList data={data} />
       </MainContent>
       <FooterMenu />
