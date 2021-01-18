b98e276d2c7a09b43f7447dd07b28bbd76148675
LeandroLino <leao.lino7@gmail.com>
2021-01-18 12:52:40 -0300

    i finish the ternary in footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import GlobalStyle from "./styles/global";
 import Routes from "./routes";
 import FooterMenu from "./components/molecules/FooterMenu";
+import { useHistory } from "react-router-dom";
 function App() {
+  const history = useHistory();
+  console.log(history);
   return (
     <div id="bg-filter">
       <GlobalStyle />
       <Routes />
-      <FooterMenu />
+      {history.location.pathname === "/login" ||
+      history.location.pathname === "/register" ||
+      history.location.pathname === "/home" ? (
+        <></>
+      ) : (
+        <FooterMenu />
+      )}
     </div>
   );
 }
