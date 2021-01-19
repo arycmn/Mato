387d6f9ec015c8ea8bf42436b760441536023db5
Bruno Gehlen <tousepc@gmail.com>
2021-01-19 20:32:06 -0300

    Feat: Add Register page desktop layout

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,14 +1,55 @@
 import Register from "../../components/molecules/Register";
 import Title from "../../components/atoms/Title";
-import { Header } from "./style";
+import { Header,Border,CommentContent,Comment } from "./style";
 import logo from "../../images/logo Mato.png";
+import SanCarousel from 'sancarousel';
+import carousel_pic1 from '../../images/carousel_pic1.jpg'
+import carousel_pic2 from '../../images/carousel_pic2.jpg'
+import carousel_pic3 from '../../images/carousel_pic3.jpg'
+import carousel_pic4 from '../../images/carousel_pic4.jpg'
 
 const RegisterPage = () => {
+  
+  
+  const slides = [
+    {
+      img: carousel_pic1,
+      url: '/link-1'
+    },
+    {
+      img: carousel_pic2,
+      url: '/link-2'
+    },
+    {
+      img: carousel_pic3,
+      url: '/link-3'
+    },
+    {
+      img: carousel_pic4,
+      url: '/link-4'
+    }
+  ];
+
   return (
     <Header>
       <img src={logo} alt={"logo"}></img>
+      <Border>
+        <SanCarousel 
+          slides={slides}
+          height={'55vh'}
+          width={'55vw'}
+          slideSize="100%"
+          autoPlay={8000}
+          spaceBetweenSlides="10px"
+         />
+      </Border>
       <Title text={"Cadastre - se"} />
-      <Register />;
+      <Register />
+
+      <Comment>
+          <img src={'https://images.pexels.com/photos/5528835/pexels-photo-5528835.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'} alt={'profile'}/>
+        <CommentContent>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi repudiandae qui assumenda aspernatur ut tenetur saepe doloribus similique nobis corrupti nostrum, quisquam at commodi minima dicta repellendus voluptates dignissimos aliquid!</CommentContent>
+      </Comment>
     </Header>
   );
 };
--- src/pages/Register/style.js
+++ src/pages/Register/style.js
@@ -18,27 +18,86 @@ export const Header = styled.div`
     width: 150px;
     height: 150px;
     padding-left: 15vh;
+    
     @media (min-width: 280px) {
       padding-left: 10vh;
     }
+    
     @media (min-width: 360px) {
       padding-left: 15vh;
     }
+    
     @media (min-width: 375px) {
       padding-left: 13vh;
     }
+    
     @media (min-width: 411px) {
       padding-left: 16vh;
     }
+    
     @media (min-width: 540px) {
       padding-left: 25vh;
     }
+
     @media (min-width: 760px) {
       padding-left: 25vh;
       padding-top: 10vw;
       padding-bottom: 10vw;
       width: 400px;
       height: 400px;
-    
   }
+
+  @media (min-width: 1000px) {
+      padding:0;
+      position:absolute;
+      top:30px;
+      left:200px;
+      width: 180px;
+      height: 180px;
+  }
+  }
+`;
+
+export const Border = styled.div`
+  @media (max-width: 1000px) {
+      display:none;
+  }  
+`;
+
+export const Comment = styled.div`
+    @media (max-width: 1000px) {
+      display:none;
+    }  
+    
+    @media (min-width: 1000px) {  
+      width:1035px;
+      height:100px;
+      background-color: rgb(14, 29, 14,0.4);
+      position:absolute;
+      top:820px;
+      left:100px;
+
+      > img {
+        width:72px;
+        height:72px;
+        position:absolute;
+        border-radius:50%;
+        margin-top:14px;
+        margin-left:14px;
+      }
+    }
+`;
+
+export const CommentContent = styled.div`
+    
+    @media (max-width: 1000px) {
+      display:none;
+    }
+
+    @media (min-width: 1000px) {
+      font-weight:200;
+      font-size:14px;
+      margin-left:110px;
+      margin-top:25px;
+    }
 `;
