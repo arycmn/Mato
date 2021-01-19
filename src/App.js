65d862d1dfcd77186b48a01bee841c89661d19d6
ingridbgr <ingridbgr@outlook.com>
2021-01-19 19:23:52 -0300

    feat: sider menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import GlobalStyle from "./styles/global";
 import Routes from "./routes";
 import FooterMenu from "./components/molecules/FooterMenu";
+import SiderMenu from "./components/molecules/SiderMenu";
 import { useHistory } from "react-router-dom";
 function App() {
   const history = useHistory();
@@ -15,7 +16,10 @@ function App() {
       history.location.pathname === "/home" ? (
         <></>
       ) : (
-        <FooterMenu />
+        <>
+          <FooterMenu />
+          <SiderMenu />
+        </>
       )}
     </>
   );
