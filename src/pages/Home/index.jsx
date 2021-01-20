9f5caad3753f40ef9e4d013bb77457bfccd6956b
LeandroLino <leao.lino7@gmail.com>
2021-01-19 22:17:00 -0300

    Fix: i fixed the title in mainContent

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,8 +7,7 @@ const Reservation = () => {
   const history = useHistory();
   return (
     <Container>
-      <Title text={`Welcome`}></Title>
-      <MainContent>
+      <MainContent title={"Welcome"}>
         <div className="Link" onClick={() => history.push(`/news`)}>
           <img alt={"Backpack-Icon"} src={Backpack}></img>
           <div>News</div>
@@ -22,12 +21,18 @@ const Reservation = () => {
           <div>Recomended Places</div>
         </Recomended>
         <ImagemContainer>
-          <div alt="Tanda Azul" onClick={() => history.push(`/location/1`)}>
-            <img src="https://cdn.pixabay.com/photo/2014/09/14/21/38/camping-445425_960_720.jpg"></img>
+          <div onClick={() => history.push(`/location/1`)}>
+            <img
+              alt="Tanda Azul"
+              src="https://cdn.pixabay.com/photo/2014/09/14/21/38/camping-445425_960_720.jpg"
+            ></img>
             <div>Tenda Azul</div>
           </div>
-          <div alt="Red Forest" onClick={() => history.push(`/location/2`)}>
-            <img src="https://images.pexels.com/photos/3551207/pexels-photo-3551207.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
+          <div onClick={() => history.push(`/location/2`)}>
+            <img
+              alt="Red Forest"
+              src="https://images.pexels.com/photos/3551207/pexels-photo-3551207.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            ></img>
             <div>Red Forest</div>
           </div>
           <div onClick={() => history.push(`/location/3`)}>
@@ -44,8 +49,11 @@ const Reservation = () => {
             ></img>
             <div>Green trail</div>
           </div>
-          <div alt="Waterfall" onClick={() => history.push(`/location/5`)}>
-            <img src="https://i.picsum.photos/id/1039/300/300.jpg?hmac=im4VlwAVlc_D5YrlPUIyevVkivYNpLovBLVBSf4WmSw"></img>
+          <div onClick={() => history.push(`/location/5`)}>
+            <img
+              alt="Waterfall"
+              src="https://i.picsum.photos/id/1039/300/300.jpg?hmac=im4VlwAVlc_D5YrlPUIyevVkivYNpLovBLVBSf4WmSw"
+            ></img>
             <div>Waterfall</div>
           </div>
         </ImagemContainer>
