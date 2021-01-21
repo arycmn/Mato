773a8bb8d9d174c6d790bd16b981c349be5bf9a1
Débora Barros <debora_lbarros@outlook.com>
2021-01-20 21:58:57 -0400

    feat: some tests and add CI

---
+++ src/components/__tests__/localActivityList.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import LocalActivtyList from "../molecules/LocalActivityList";
+
+describe("Local Activity", () => {
+  test("Should show an activity", () => {
+    const id = [2, 5, 4];
+
+    render(<LocalActivtyList activityID={id} />);
+
+    const fromScreen = screen.getByText("Board");
+    const img = screen.getAllByRole("img");
+
+    expect(fromScreen).toBeInTheDocument();
+    expect(img).toHaveLength(3);
+  });
+});
