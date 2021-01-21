7bc8df7ff0b595804b40ea9de8a7a7cedf4c6923
ingridbgr <ingridbgr@outlook.com>
2021-01-21 10:33:32 -0300

    fix: validations

--- src/pages/ProfileSettings/validations.js
+++ src/pages/ProfileSettings/validations.js
@@ -6,12 +6,10 @@ export const schema = yup.object().shape({
     .string()
     .min(3, "O nome deve conter no mínimo 3 letras")
     .required("Campo obrigatorio"),
-  bio: yup.string().required("Campo obrigatorio"),
-  contact: yup.string().required("Campo obrigatorio"),
+
   password: yup.string().optional(),
   password_confirm: yup
     .string()
     .optional()
     .oneOf([yup.ref("password")], "A senha deve ser igual"),
-  course_module: yup.string().required("Campo obrigatorio"),
 });
