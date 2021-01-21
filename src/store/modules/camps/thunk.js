eaea103d4babdc7e6f001efddc3b9e91b4b42afb
LeandroLino <leao.lino7@gmail.com>
2021-01-21 01:49:28 -0300

    Were no changes, only identation

--- src/store/modules/camps/thunk.js
+++ src/store/modules/camps/thunk.js
@@ -1,28 +1,22 @@
-import { storeCamp,storeCamps } from './actions'
-import API from '../../../services/axios'
+import { storeCamp, storeCamps } from "./actions";
+import API from "../../../services/axios";
 
-  export const getCampsites = () => async (dispatch) => {
-    
-    try {
-      const response = await API.get(`/campsite`)
-      const fetchedCamps = response.data
-      dispatch(storeCamps(fetchedCamps))
-
-    } catch (err) {
-      console.log(err)
-
-    }
+export const getCampsites = () => async (dispatch) => {
+  try {
+    const response = await API.get(`/campsite`);
+    const fetchedCamps = response.data;
+    dispatch(storeCamps(fetchedCamps));
+  } catch (err) {
+    console.log(err);
   }
+};
 
-  export const getCampsiteByCampsiteId = (campsiteId) => async (dispatch) => {
-  
-    try {
-      const response = await API.get(`/campsite/${campsiteId}`)
-      const fetchedCamp = response.data
-      dispatch(storeCamp(response.data))
-      
-    } catch (err) {
-      console.log(err)
-
-    }
+export const getCampsiteByCampsiteId = (campsiteId) => async (dispatch) => {
+  try {
+    const response = await API.get(`/campsite/${campsiteId}`);
+    const fetchedCamp = response.data;
+    dispatch(storeCamp(response.data));
+  } catch (err) {
+    console.log(err);
   }
+};
